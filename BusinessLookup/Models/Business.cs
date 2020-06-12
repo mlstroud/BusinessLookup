65834d14a4ac2d209de3379b9234eb3937d39b42
Matt Stroud <lixingke06@gmail.com>
2020-06-12 08:45:04 -0700

    add business properties

--- BusinessLookup/Models/Business.cs
+++ BusinessLookup/Models/Business.cs
@@ -2,6 +2,11 @@ namespace BusinessLookup.Models
 {
   public class Business
   {
-
+    public int BusinessId { get; set; }
+    public string Name { get; set; }
+    public string Category { get; set; }
+    public string Description { get; set; }
+    public string OpeningTime { get; set; }
+    public string ClosingTime { get; set; }
   }
 }
