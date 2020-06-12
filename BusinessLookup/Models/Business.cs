c076b00384f2e2b043eb4cb48e6763b2509d0b0a
Matt Stroud <lixingke06@gmail.com>
2020-06-12 10:48:29 -0700

    add Address Property

--- BusinessLookup/Models/Business.cs
+++ BusinessLookup/Models/Business.cs
@@ -6,6 +6,7 @@ namespace BusinessLookup.Models
     public string Name { get; set; }
     public string Category { get; set; }
     public string Description { get; set; }
+    public string Address { get; set; }
     public string OpeningTime { get; set; }
     public string ClosingTime { get; set; }
   }
