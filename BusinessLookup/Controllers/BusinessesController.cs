572073f95894d55f0f15a4cb69c4f5721f50f6a5
Matt Stroud <lixingke06@gmail.com>
2020-06-12 09:26:01 -0700

    add businessescontroller

---
+++ BusinessLookup/Controllers/BusinessesController.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using BusinessLookup.Models;
+
+namespace BusinessLookup.Controllers
+{
+  public class BusinessesController : ControllerBase
+  {
+
+  }
+}
