541750be8f41ba36c87e713b027ff71c76193b2c
Matt Stroud <lixingke06@gmail.com>
2020-06-12 09:09:03 -0700

    include Models

--- BusinessLookup/Startup.cs
+++ BusinessLookup/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Microsoft.EntityFrameworkCore;
+using BusinessLookup.Models;
 
 namespace BusinessLookup
 {
