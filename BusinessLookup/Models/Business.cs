df6f1aa3437ca1243b980c9e3984bb586d47f14e
Matt Stroud <lixingke06@gmail.com>
2020-06-12 11:05:05 -0700

    add validations and error messages to properties

--- BusinessLookup/Models/Business.cs
+++ BusinessLookup/Models/Business.cs
@@ -1,13 +1,30 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace BusinessLookup.Models
 {
   public class Business
   {
     public int BusinessId { get; set; }
+
+    [Required]
+    [StringLength(30)]
     public string Name { get; set; }
+
+    [Required]
     public string Category { get; set; }
+
+    [Required]
     public string Description { get; set; }
+
+    [Required]
     public string Address { get; set; }
+
+    [Required]
+    [StringLength(7, ErrorMessage = "Time format must be HH:MMam / pm or H:MMam / pm.")]
     public string OpeningTime { get; set; }
+
+    [Required]
+    [StringLength(7, ErrorMessage = "Time format must be HH:MMam/pm or H:MMam/pm.")]
     public string ClosingTime { get; set; }
   }
 }
